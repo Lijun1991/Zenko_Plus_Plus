a65e269696e76f5a5cc5bf07b4157a69195b491f
Lijun Wang <wang283808065@gmail.com>
2017-08-16 22:00:14 -0700

    metadata saves in leveldb, get/put works, need to save data to google

--- mdserver.js
+++ mdserver.js
@@ -2,7 +2,24 @@
 
 const arsenal = require('arsenal');
 const werelogs = require('werelogs');
-const Memcached = require('memcached');
+
+const fs = require('fs');
+const assert = require('assert');
+const uuid = require('uuid');
+const level = require('level');
+const sublevel = require('level-sublevel');
+const debug = require('debug')('MetadataFileServer');
+const diskusage = require('diskusage');
+const Logger = require('werelogs').Logger;
+const errors = arsenal.errors;
+const constants = arsenal.constants;
+const levelNet = arsenal.network.level;
+const WGM = require('../arsenal/lib/versioning/WriteGatheringManager');
+const WriteCache = require('../arsenal/lib/versioning/WriteCache');
+const VRP = require('../arsenal/lib/versioning/VersioningRequestProcessor');
+
+const ROOT_DB = 'rootDB';
+const SYNC_OPTIONS = { sync: true };
 
 const SUBLEVEL_SEP = '::';
 const MEMCACHED_LIFETIME = 100000;
@@ -26,92 +43,108 @@ recordLog: { enabled: false, recordLogName: 's3-recordlog' },
 versioning: { replicationGroupId: 'RG001' },
 log: logOptions });
 
-var memcached = new Memcached('localhost:11211', {retries:10,retry:10000,remove:true,failOverServers:['192.168.0.103:11211']});
-
-class MemcachedService extends arsenal.network.rpc.BaseService {
-    constructor(params) {
-	super(params);
-	this.addRequestInfoConsumer((dbService, reqParams) => {
-            const env = {};
-            env.subLevel = reqParams.subLevel;
-	    return env;
+mdServer.initMetadataService = function () {
+	// all metadata operations executed by leveldb go through the
+	// /metadata namespace
+	const namespace = `${constants.metadataFileNamespace}/metadata`;
+	this.logger.info(`creating metadata service at ${namespace}`);
+	this.rootDb = sublevel(level(`${this.path}/${ROOT_DB}`));
+	const dbService = new levelNet.LevelDbService({
+		rootDb: this.rootDb,
+		namespace,
+		logger: this.logger,
 	});
-    }
+	this.services.push(dbService);
+
+	/* provide an API compatible with MetaData API */
+	const metadataAPI = {
+		get: (request, logger, callback) => {
+			const dbPath = request.db.split(SUBLEVEL_SEP);
+			const subDb = dbService.lookupSubLevel(dbPath);
+			subDb.get(request.key, (err, data) => {
+				if (err && err.notFound) {
+					return callback(errors.ObjNotFound);
+				}
+				return callback(err, data);
+			});
+		},
+		list: (request, logger, callback) => {
+			const dbPath = request.db.split(SUBLEVEL_SEP);
+			const subDb = dbService.lookupSubLevel(dbPath);
+			const stream = subDb.createReadStream(request.params);
+			const res = [];
+			let done = false;
+			stream.on('data', data => res.push(data));
+			stream.on('error', err => {
+				if (done === false) {
+					done = true;
+					callback(err);
+				}
+			});
+			stream.on('end', () => {
+				if (done === false) {
+					done = true;
+					callback(null, res);
+				}
+			});
+		},
+		batch: (request, logger, callback) => {
+			const dbPath = request.db.split(SUBLEVEL_SEP);
+			const ops = request.array.map(
+				op => Object.assign({}, op, { prefix: dbPath }));
+			if (this.recordLog.enabled) {
+				this.recordLogService
+					.withRequestParams(
+						{ logName: this.recordLog.recordLogName })
+					.createLogRecordOps(
+						ops, (err, logEntries) => {
+							debug('ops to batch:', ops);
+							debug('log entries:', logEntries);
+							return this.rootDb.batch(
+								ops.concat(logEntries), SYNC_OPTIONS,
+								err => callback(err));
+						});
+			} else {
+				this.rootDb.batch(ops, SYNC_OPTIONS,
+								  err => callback(err));
+			}
+		},
+	};
+
+	Object.keys(metadataAPI).forEach(k => {
+		metadataAPI[k] = metadataAPI[k].bind(dbService);
+	});
+
+	const wgm = new WGM(metadataAPI);
+	const writeCache = new WriteCache(wgm);
+	const vrp = new VRP(writeCache, wgm, this.versioning);
+
+	dbService.registerAsyncAPI({
+		put: (env, key, value, options, cb) => {
+			console.log('put hahahah');
+			const dbName = env.subLevel.join(SUBLEVEL_SEP);
+			vrp.put({ db: dbName, key, value, options },
+					env.requestLogger, cb);
+		},
+		del: (env, key, options, cb) => {
+			const dbName = env.subLevel.join(SUBLEVEL_SEP);
+			vrp.del({ db: dbName, key, options },
+					env.requestLogger, cb);
+		},
+		get: (env, key, options, cb) => {
+			console.log('get hahahah');
+			const dbName = env.subLevel.join(SUBLEVEL_SEP);
+			vrp.get({ db: dbName, key, options },
+					env.requestLogger, cb);
+		},
+		getDiskUsage: (env, cb) => diskusage.check(this.path, cb),
+	});
+	dbService.registerSyncAPI({
+		createReadStream:
+		(env, options) => env.subDb.createReadStream(options),
+		getUUID: () => this.readUUID(),
+	});
 }
 
-mdServer.initMetadataService = function ()
-{
-    const dbService = new MemcachedService({
-	namespace: '/MDFile/metadata',
-	logger: logger
-    });
-    this.services.push(dbService);
-
-    dbService.registerAsyncAPI({
-        put: (env, key, value, options, cb) => {
-	    const dbName = env.subLevel.join(SUBLEVEL_SEP);
-	    console.log('lolololput',env,dbName,key,value,options);
-/*	    memcached.get(dbName, (err, data) => {
-		if (err) {
-		    console.log(err);
-		    let db = {};
-		    db[key] = value;
-		    memcached.add(dbName, JSON.stringify(db), MEMCACHED_LIFETIME, 
-				  (err) => {
-				      if (err) {
-					  console.log(err);
-					  cb(err);
-				      } else {
-					  cb(null);
-				      }
-				  });
-		} else {
-		    console.log(data);
-		    let db = JSON.parse(data);
-		    db[key] = value;
-		    memcached.replace(dbName, JSON.stringify(db), MEMCACHED_LIFETIME, 
-				      (err) => {
-					  if (err) {
-					      console.log(err);
-					      cb(err);
-					  } else {
-					      cb(null);
-					  }
-				      });
-		}
-		});*/
-			return cb();
-        },
-        del: (env, key, options, cb) => {
-	    console.log('del',env,key,options);
-        },
-        get: (env, key, options, cb) => {
-	    console.log('hahahhaget',key,options);
-/*	    memcached.get(dbName, (err, data) => {
-		if (err) {
-		    console.log(err);
-		} else {
-		    console.log(data);
-		    let db = JSON.parse(data);
-		    cb(null, db[key])
-		}
-	    });
-*/
-		return cb();
-    },
-	getDiskUsage: (env, cb) => {
-	    console.log('getDiskUsage',env);
-	},
-    });
-    dbService.registerSyncAPI({
-        createReadStream:
-        (env, options) => {
-	    console.log('createReadStream');
-	},
-        getUUID: () => this.readUUID(),
-    });
-    
-    console.log('Hooks installed');
-}
 
 mdServer.startServer();
